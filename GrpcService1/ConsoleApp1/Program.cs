3abae9dec85951d11a30c47aa03c675bae02542d
JohnyWinchester <66486372+JohnyWinchester@users.noreply.github.com>
2023-02-04 18:26:32 +0300

    git

---
+++ GrpcService1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Net.Client;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine();
+            GrpcTest();
+            Console.ReadKey();
+        }
+
+        static async void GrpcTest()
+        {
+            AppContext.SetSwitch(
+                    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var httpHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5241",
+                new GrpcChannelOptions { HttpHandler = httpHandler });
+            // создаем клиент
+            var client = new Values.ValuesClient(channel);
+
+            var response = client.GetValues(new RequestValues());
+
+            var values = response.ResponseStream;
+
+            while (await values.MoveNext(new CancellationToken()))
+            {
+                SendValues sendValues = values.Current;
+                Console.WriteLine(sendValues.Value);
+            }
+
+            var responseCount = client.Count(new RequestCount());
+            Console.WriteLine(responseCount.Count);
+        }
+    }
+}
---
+++ GrpcService1/GrpcService1/Services/ValuesService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+
+namespace GrpcService1.Services
+{
+    public class ValuesService: Values.ValuesBase
+    {
+        private readonly Dictionary<int, string> _Values = Enumerable.Range(1, 10)
+            .Select(i => (Id: i, Value: $"Value-{i}"))
+            .ToDictionary(v => v.Id, v => v.Value);
+
+        public override Task<SendCount> Count(RequestCount request, ServerCallContext context)
+        {
+            return Task.FromResult(new SendCount { Count = _Values.Count });
+        }
+
+        public override async Task GetValues(
+            RequestValues request, 
+            IServerStreamWriter<SendValues> responseStream, 
+            ServerCallContext context)
+        {
+            foreach(var el in _Values)
+            {
+                await responseStream.WriteAsync( new SendValues { Value = el.Value });
+            }
+        }
+    }
+}
